bc6879a38c15184ae8ea62f02bfdf03b68f12b68
Vamsi Devalla <vammichanti40312@gmail.com>
2018-02-13 19:38:13 -0500

    intial setup for bootstrap, font-awesome and firebase done

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD4oZjaeSenW_tFpCmYv4fGhoFcvbXdB9Y",
+    authDomain: "myfirstangular5.firebaseapp.com",
+    databaseURL: "https://myfirstangular5.firebaseio.com",
+    projectId: "myfirstangular5",
+    storageBucket: "myfirstangular5.appspot.com",
+    messagingSenderId: "321900797094"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD4oZjaeSenW_tFpCmYv4fGhoFcvbXdB9Y",
+    authDomain: "myfirstangular5.firebaseapp.com",
+    databaseURL: "https://myfirstangular5.firebaseio.com",
+    projectId: "myfirstangular5",
+    storageBucket: "myfirstangular5.appspot.com",
+    messagingSenderId: "321900797094"
+  }
 };
